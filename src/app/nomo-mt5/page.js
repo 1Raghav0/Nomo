0db54824235bcc11aa6c2272f62c57449f7bc45c
1Raghav0 <raghav514422@gmail.com>
2025-10-20 22:49:33 +0530

    Full nomo-mt5 page created

---
+++ src/app/nomo-mt5/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import WhyCopyTrading from './WhyCopyTrading'
+import FaqSection from './FaqSection'
+
+const page = () => {
+  return (
+    <>
+    <HeroSection />
+    <WhyCopyTrading />
+    <FaqSection />
+    </>
+  )
+}
+
+export default page
